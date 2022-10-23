24d6c9c2be0ac6823daad3a23ccd0e55101b1be3
k19880925@gmail.com <k19880925@gmail.com>
2022-10-23 15:34:42 +0900

    test

--- Server/index.js
+++ Server/index.js
@@ -4,22 +4,33 @@ import express, { json } from "express";
 import getTokenUri from "./apps/tokenUri.js";
 const app = express();
 app.use(express.json());
+app.listen(3000);
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-//@ Get the token uri from IPFS
+//@ Image 정보를 받아, uri 형식으로 전달
 app.post("/tokenuri", (req, res) => {
   const dataAddr = req.body.data;
-  getTokenUri(dataAddr).then(
-    (e) => {
+  const stopdata = { network: 1, preload: 2, ipns: 3, repo: 4, mfsPreload: 5 };
+  const test = getTokenUri(dataAddr)
+    .then((e) => {
       res.send(e);
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+// 저장 따로 전송 따로 ?
+//@ Data 저장 ??
+app.post("/getthedata", (req, res) => {
+  //console.log(req.body.data);
+
+  res.send(req.body.data);
 });
 
-app.listen(3000);
+//@ Data 전송
+app.get("/senddata", (req, res) => {
+  res.send("data를 받으세요 ");
+});
