9205e422e79d4d7434a6763861e814174f8553fd
k19880925@gmail.com <k19880925@gmail.com>
2022-10-23 14:47:12 +0900

    update_tokenUri

--- Server/apps/tokenUri.js
+++ Server/apps/tokenUri.js
@@ -1,12 +1,11 @@
 import * as IPFS from "ipfs-core";
 import fs from "fs";
-
+const node = await IPFS.create();
 export default async function getTokenUri(uri) {
   let test = "test";
-  const node = await IPFS.create();
   //const image = fs.readFileSync("./Image/doge1.jpg");
-  const image = fs.readFileSync(uri);
-  const fileAdded = await node.add(image);
+  //const image = fs.readFileSync(uri);
+  const fileAdded = await node.add(uri);
   const initailUri = "https://ipfs.io/ipfs/";
   test = initailUri + fileAdded.cid;
   return test;
